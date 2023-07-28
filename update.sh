ba1c72b396a74d936f2f0719ff8af8d201f14a87
Frz-Ehr <136071031+Frz-Ehr@users.noreply.github.com>
2023-07-28 11:43:57 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Replace with your GitHub username and repository name
+GITHUB_USER="Frz-Ehr"
+REPO_NAME="raspwidget"
+
+# Directory where the repository should be cloned
+DIR="/home/hug/Desktop/${REPO_NAME}"
+
+# Check if the directory already exists
+if [ -d "$DIR" ]; then
+  # If the directory exists, navigate to it and pull the latest changes
+  cd "$DIR" || exit
+  git fetch
+  git reset --hard origin/main
+else
+  # If the directory does not exist, clone the repository
+  git clone "https://github.com/${GITHUB_USER}/${REPO_NAME}.git" "$DIR"
+fi
+
+exit
