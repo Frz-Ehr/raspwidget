9c6d66cf8915a961d6b3e03d02322215c0f6ea76
Frz-Ehr <136071031+Frz-Ehr@users.noreply.github.com>
2023-08-02 19:15:57 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import importlib
+
+WIDGETS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'widgets')  # Absolute path
+
+def load_widget(widget_name):
+    print("Loading widget: " + widget_name)  # Point de contrôle
+    sys.path.insert(0, WIDGETS_DIR)  # Add WIDGETS_DIR to the module search path
+    module = importlib.import_module(f"{widget_name}.{widget_name}")  # Remove "widgets." prefix
+    WidgetClass = getattr(module, 'Widget')
+    print("Widget class obtained")  # Point de contrôle
+    widget = WidgetClass()
+    print("Widget instance created")  # Point de contrôle
+    return widget
+
+# Test the function
+widget = load_widget('btc-price')
