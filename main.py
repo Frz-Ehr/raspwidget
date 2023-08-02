7884eebbc8349dcd4464e9825cc91d99d6357d70
Frz-Ehr <136071031+Frz-Ehr@users.noreply.github.com>
2023-08-02 18:30:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import importlib
 import tkinter as tk
 from tkinter import simpledialog, Toplevel, Listbox
 
-
-
 WIDGETS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'widgets')
 
 available_widgets = [f[:-3] for f in os.listdir(WIDGETS_DIR) if f.endswith('.py') and f != '__init__.py']
@@ -27,7 +25,7 @@ def choose_widget(frame):
             new_widget = load_widget(widget_name).get_tk_object()
             for widget in frame.winfo_children():
                 widget.destroy()
-            new_widget.pack()
+            new_widget.pack(fill='both', expand=True)  # Modify here
             top.destroy()
         except Exception as e:
             print(f"Error while loading widget: {str(e)}")
@@ -45,13 +43,19 @@ def choose_widget(frame):
 root = tk.Tk()
 root.geometry("800x480")
 
+# Configure the grid
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+
 frames = []
 for i in range(2):
     for j in range(2):
         frame = tk.Frame(root)
-        frame.grid(row=i, column=j)
+        frame.grid(row=i, column=j, sticky='nsew')  # Modify here
         frames.append(frame)
         button = tk.Button(frame, text="Choose widget", command=lambda frame=frame: choose_widget(frame))
-        button.pack()
+        button.pack(fill='both', expand=True)  # Modify here
 
 root.mainloop()
