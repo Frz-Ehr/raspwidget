227d8ad865e3582f5506c8a0bfb8661d443bd20d
Frz-Ehr <136071031+Frz-Ehr@users.noreply.github.com>
2023-08-02 18:41:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ WIDGETS_DIR = 'widgets'  # Relative path
 available_widgets = [f[:-3] for f in os.listdir(WIDGETS_DIR) if f.endswith('.py') and f != '__init__.py']
 
 def load_widget(widget_name):
-    module = importlib.import_module(f"widgets.{widget_name}")  # Change this line
+    sys.path.insert(0, WIDGETS_DIR)  # Add WIDGETS_DIR to the module search path
+    module = importlib.import_module(f"{widget_name}")  # Remove "widgets." prefix
     WidgetClass = getattr(module, 'Widget')
     widget = WidgetClass()
     return widget
